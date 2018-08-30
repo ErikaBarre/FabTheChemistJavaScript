7966576448d3a2d5b59c774b662a0fd8a54d7806
fabrice <fabrice@schopenhauer>
2018-08-31 00:24:08 +0200

    44545

--- FabTheChemistJavaScript/nodejs/07_reusability/notes.js
+++ FabTheChemistJavaScript/nodejs/07_reusability/notes.js
@@ -6,56 +6,66 @@ console.log('Starting notes.js');
 const fs = require('fs');
 
 var fetchNodes = () => {
-	try{
-		var noteString = fs.readFileSync('notes-data.json');
-		return JSON.parse(noteString);
-	}catch(exception){
-		console.log('nL23');
-		return [];
-	}
+    try {
+        var noteString = fs.readFileSync('notes-data.json');
+        return JSON.parse(noteString);
+    } catch (exception) {
+        console.log('nL23');
+        return [];
+    }
 };
 
 var saveNotes = (notes) => {
-	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-	
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
 };
 
 var addNote = (title, body) => {
-	
-	var notes = fetchNodes();
-	
-	var note = {
-		title,
-		body
-	};
-	
-	
-	var duplicateNotes = notes.filter((note) => note.title === title); 
-	console.log('nL34', duplicateNotes);
-	if(duplicateNotes.length === 0){
-		console.log('L35');
-		notes.push(note);
-		saveNotes(notes);
-	}
-} ;
+
+    var notes = fetchNodes();
+
+    var note = {
+        title,
+        body
+    };
+
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    console.log('nL34', duplicateNotes);
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        try {
+            saveNotes(notes);
+            return note;
+        } catch (exception) {
+            return 'ERROR_SAVE';
+        }
+
+    } else {
+        return 'DUPLICATED_DATA';
+    }
+};
 
 var getAll = () => {
-	console.log('get all notes');
-} ;
+    console.log('get all notes');
+};
 
 var getNoteAll = (title) => {
-	console.log('get note :' , title);
-} ;
+    console.log('get note :', title);
+};
 
 var removeNote = (title) => {
-	console.log('remove note :' , title);
-} ;
+    var notes = fetchNodes();
+    var filteredNotes = notes.filter((note) =>     note.title !== title);
+    //fs.truncate('notes-data.json', 0, function(){console.log('done')})
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
+    
+};
 
 module.exports = {
-	addNote,
-	getAll,
+    addNote,
+    getAll,
     getNoteAll,
     removeNote
 };
-
-
